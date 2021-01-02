e17dc57c239d0a6a41bb8606a52564a50e3800f7
Tripleteam88 <71329147+Tripleteam88@users.noreply.github.com>
2021-01-02 17:54:04 -0500

    successfull import of gcf function form file in subdirectory

---
+++ ImportTest.py
@@ -0,0 +1,11 @@
+'''
+
+Testing importing of functions from other files
+
+'''
+
+
+
+from Math.Factors import gcf #This line seems to work when importing from a file within a subdirectory
+
+gcf(10000, 82734890)
--- Math/ImportTest.py
+++
@@ -1,9 +0,0 @@
-'''
-Testing importing of functions from other files
-
-'''
-
-
-from Factors import gcf
-
-gcf(10000, 82734890)
